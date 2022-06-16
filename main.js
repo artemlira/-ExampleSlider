65e7332995a6707b76eb43eb3778f6008404fe04
Lira Artem <artemlira84@gmail.com>
2022-06-16 21:02:23 +0300

    исправил ошибки, реализовал анимацию через transition

--- main.js
+++ main.js
@@ -2,107 +2,77 @@
 
 
 class ExampleSlider {
-   constructor({ wrapper, slider, time = 2000, }) {
+   constructor({ wrapper, slider, sliders, time = 1500, }) {
       this.wrapper = document.querySelector(wrapper);
       this.slider = this.wrapper.querySelector(slider);
+      this.sliders = this.slider.querySelectorAll(sliders);
       this.width = this.slider.querySelector('ul').clientWidth;
       this.time = time;
+      this.index = 0;
    }
 
    hideAllSlide() {
-      this.slider.querySelectorAll('li').forEach(v => v.classList.add('hide'));
+      this.sliders.forEach(v => {
+         v.classList.add('hide');
+         v.classList.remove('toright');
+      });
    }
 
    createStyle() {
       let style =
          `<style>
                   .hide{
-                     display:none;
-                  }
-
-                  .furtherLeft{
-
-                     animation-name : hide;
-                     animation-duration:${this.time}ms;
+                     right:-${this.width}px;
+                     opacity:0;
                   }
 
                   .toleft{
-                     display: block;
-                     z-index: 20;
-                     animation-name : show;
-                     animation-duration:${this.time}ms;
+                     right: 0;
+                     opacity:1;
+                     transition-property: right, opacity;
+                     transition-duration: ${this.time}ms;
                   }
 
-                  @keyframes hide {
-                     0%{
-                        right:0;
-                        opaity:1;
-                     }
-                     25%{
-                         opaity:0.2;
-                     }
-                     100%{
-                        right:${this.width}px;
-                        opacity:0;
-                     }
-                  }
-
-                  @keyframes show {
-                     0%{
-                        right:-${this.width}px;
-                        opacity:0;
-                     }
-                     50%{
-                        opaity:0;
-                     }
-                     75%{
-                        opaity:0.3;
-                     }
-                     100%{
-                        right:0;
-                        opaity:1;
-                     }
+                  .toright{
+                     right:${this.width}px;
+                     opacity:0;
+                     transition-property: right, opacity;
+                     transition-duration: ${this.time}ms, ${this.time / 3}ms;
                   }
          </style>`
       document.head.insertAdjacentHTML('beforeend', style);
    }
 
-   showSlide(elem) {
-      elem.classList.remove('hide');
-      elem.classList.add('toleft');
-      // setTimeout(() => {
-      //    elem.classList.add('furtherLeft');
-      //    elem.classList.remove('toleft');
-
-      //    elem.style.right = this.width + 'px';
-      // }, this.time);
+   showSlide(n) {
+      this.sliders[n].classList.remove('hide');
+      this.sliders[n].classList.add('toleft');
+      setTimeout(() => {
+         this.sliders[n].classList.add('toright');
+         this.sliders[n].classList.remove('toleft');
+         this.sliders[n].classList.add('hide');
+      }, this.time);
    }
 
    slide() {
-
-      let allSlides = this.slider.querySelectorAll('li');
-      for (let i = 0; i < allSlides.length; i++) {
-
-         // allSlides[i].style.display = 'block';
-         // allSlides[i].style.zIndex = '10';
-         this.showSlide(allSlides[i]);
-         if (allSlides[i].nextElementSibling == null) {
-            // this.showSlide(allSlides[i]);
-            console.dir(allSlides[i]);
-         }
-
+      if (this.index == this.sliders.length - 1) {
+         this.index = 0;
+         this.showSlide(this.index);
+      } else {
+         this.index++;
+         this.showSlide(this.index);
       }
    }
 
-
-
+   interval() {
+      this.hideAllSlide();
+      this.slide();
+   }
 
    init() {
       console.dir(this);
       this.hideAllSlide();
       this.createStyle();
-      // this.showSlide(this.slider.querySelector('li'));
-      this.slide();
+      setInterval(() => { this.interval() }, 2000);
    }
 }
 
@@ -111,7 +81,7 @@ class ExampleSlider {
 let data = {
    wrapper: '.wrapper',
    slider: '.slider',
-
+   sliders: 'li',
 };
 
 const slider = new ExampleSlider(data);
